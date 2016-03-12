5e8b023ecc580654da739b1c95fc44ab466283ff
tswestendorp <contact@tswestendorp.nl>
2016-03-12 19:08:20 +0100

    Initial Commit

---
+++ tests/integration/message/MessageTest.php
@@ -0,0 +1,55 @@
+<?php
+
+class MessageTest extends BaseTest
+{
+    public function testConstructor()
+    {
+        $message = new \BulkSmsCenter\Message();
+        $this->assertInstanceOf("{$this->namespace}\Message",$message);
+    }
+
+    public function testConstructorWithOptions()
+    {
+        $message = new \BulkSmsCenter\Message($options = [
+            'key1' => 'value1',
+            'key2' => 'value2',
+        ]);
+        $this->assertEquals($options,$message->getOptions());
+    }
+
+    public function testConstructorWithSetterOption()
+    {
+        $message = new \BulkSmsCenter\Message($options = [
+            $key = 'id' => str_random(32),
+        ]);
+        $this->assertEquals($options[$key],$message->getId());
+    }
+
+    public function testSetGetId()
+    {
+        $message = new \BulkSmsCenter\Message();
+        $message->setId($expected = str_random(32));
+        $this->assertEquals($expected,$message->getId());
+    }
+
+    public function testSetGetBody()
+    {
+        $message = new \BulkSmsCenter\Message();
+        $message->setBody($expected = str_random(160));
+        $this->assertEquals($expected,$message->getBody());
+    }
+
+    public function testSetGetRecipient()
+    {
+        $message = new \BulkSmsCenter\Message();
+        $message->setRecipient($expected = '3161234567890');
+        $this->assertEquals($expected,$message->getRecipient());
+    }
+
+    public function testSetGetSender()
+    {
+        $message = new \BulkSmsCenter\Message();
+        $message->setSender($expected = '3161234567890');
+        $this->assertEquals($expected,$message->getSender());
+    }
+}

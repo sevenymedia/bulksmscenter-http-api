a638d4a05b4f652c01a0545331469ed014892371
tswestendorp <contact@tswestendorp.nl>
2016-03-12 20:22:57 +0100

    Removed commented spl_autoload and added a comment line

--- autoload.php
+++ autoload.php
@@ -3,29 +3,7 @@
 // Load dependencies
 require __DIR__.DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload.php';
 
-//spl_autoload_register(function ($class) {
-//    $prefix = 'BulkSmsCenter\\';
-//    $baseDirectory = __DIR__.'/src/BulkSmsCenter/';
-//
-//    // Check if class uses prefix
-//    if (strncmp($prefix,$class,$len = strlen($prefix)) !== 0) {
-//        return;
-//    }
-//
-//    $relativeClass = substr($class,$len);
-//
-//    // replace the namespace prefix with the base directory, replace namespace
-//    // separators with directory separators in the relative class name, append
-//    // with .php
-//    $file = $baseDirectory.str_replace('\\','/',$relativeClass).'.php';
-//
-//    // if the file exists, require it
-//    if (file_exists($file)) {
-//        require $file;
-//    }
-//});
-
-
+// Load Dotenv\Dotenv if it's available
 if (class_exists('Dotenv\Dotenv',true)) {
     try {
         (new Dotenv\Dotenv(__DIR__))->load();

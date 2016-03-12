1505e9843ccdf379673293ab1f7f8ee4414549d1
tswestendorp <contact@tswestendorp.nl>
2016-03-12 18:14:09 +0100

    Added three tests

--- tests/integration/auth/AuthTest.php
+++ tests/integration/auth/AuthTest.php
@@ -12,4 +12,24 @@ class AuthTest extends BaseTest
         parent::setup();
         $this->client = new \BulkSmsCenter\Client($this->mockAuth,$this->mockClient);
     }
+
+    public function testConstructor()
+    {
+        $auth = new \BulkSmsCenter\Auth('YOUR_USERNAME','YOUR_PASSWORD');
+        $this->assertInstanceOf('BulkSmsCenter\Auth',$auth);
+    }
+
+    public function testGetPassword()
+    {
+        $password = 'YOUR_PASSWORD';
+        $auth = new \BulkSmsCenter\Auth('YOUR_USERNAME',$password);
+        $this->assertEquals($password,$auth->getPassword());
+    }
+
+    public function testGetUsername()
+    {
+        $username = 'YOUR_USERNAME';
+        $auth = new \BulkSmsCenter\Auth($username,'YOUR_PASSWORD');
+        $this->assertEquals($username,$auth->getUsername());
+    }
 }

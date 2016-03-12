605c0054de48425d3bbef9744a510b956c37fd76
tswestendorp <contact@tswestendorp.nl>
2016-03-12 20:37:08 +0100

    Wrapped helpers in if-statement to check if it isn't already defined

--- src/helpers.php
+++ src/helpers.php
@@ -1,38 +1,42 @@
 <?php
 
-function env($key,$default = null) {
-    $value = getenv($key);
-    if ($value === false) {
-        return value($default);
-    }
+if (!function_exists('env')) {
+    function env($key,$default = null) {
+        $value = getenv($key);
+        if ($value === false) {
+            return value($default);
+        }
 
-    switch (strtolower($value)) {
-        case 'true':
-        case '(true)':
-            return true;
-        case 'false':
-        case '(false)':
-            return false;
-        case 'empty':
-        case '(empty)':
-            return '';
-        case 'null':
-        case '(null)':
-            return;
-    }
+        switch (strtolower($value)) {
+            case 'true':
+            case '(true)':
+                return true;
+            case 'false':
+            case '(false)':
+                return false;
+            case 'empty':
+            case '(empty)':
+                return '';
+            case 'null':
+            case '(null)':
+                return;
+        }
 
-    if ($value[0] === '"' && $value[strlen($value)-1] === '"') {
-        return substr($value,1,-1);
+        if ($value[0] === '"' && $value[strlen($value)-1] === '"') {
+            return substr($value,1,-1);
+        }
+        return $value;
     }
-    return $value;
 }
 
-function str_random($length = 16) {
-    $string = '';
-    while (($len = strlen($string)) < $length) {
-        $size = $length - $len;
-        $bytes = openssl_random_pseudo_bytes($size);
-        $string .= substr(str_replace(['/','+','='],'',base64_encode($bytes)),0,$size);
+if (!function_exists('str_random')) {
+    function str_random($length = 16) {
+        $string = '';
+        while (($len = strlen($string)) < $length) {
+            $size = $length - $len;
+            $bytes = openssl_random_pseudo_bytes($size);
+            $string .= substr(str_replace(['/','+','='],'',base64_encode($bytes)),0,$size);
+        }
+        return $string;
     }
-    return $string;
 }

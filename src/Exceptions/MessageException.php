e3b3ee6a6e5ff67723c2d0d949d5ba5af3ea2436
tswestendorp <contact@tswestendorp.nl>
2016-03-13 20:29:01 +0100

    Added some constants for codes/messages

--- src/Exceptions/MessageException.php
+++ src/Exceptions/MessageException.php
@@ -7,4 +7,9 @@
  */
 class MessageException extends BulkSmsCenterException
 {
+    const CODE__INVALID_API_CODE = 1000;
+    const CODE__INVALID_ROUTE = 1001;
+
+    const MESSAGE__INVALID_API_CODE = ClientException::MESSAGE__INVALID_API_CODE;
+    const MESSAGE__INVALID_ROUTE = 'Invalid route';
 }

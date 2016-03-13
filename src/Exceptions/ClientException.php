1cd8e25d17d2b1b5c1635fdbc9cd058c94cc7995
tswestendorp <contact@tswestendorp.nl>
2016-03-13 20:17:20 +0100

    Added some codes and messages

--- src/Exceptions/ClientException.php
+++ src/Exceptions/ClientException.php
@@ -7,4 +7,9 @@
  */
 class ClientException extends BulkSmsCenterException
 {
+    const CODE__INVALID_API_CODE = 1000;
+    const CODE__NO_MESSAGE_SET = 1001;
+
+    const MESSAGE__INVALID_API_CODE = 'Got an invalid API code (%s)';
+    const MESSAGE__NO_MESSAGE_SET = 'No message set';
 }

7df905a2e5425ef58aaeb21f14f3d778c3539832
tswestendorp <contact@tswestendorp.nl>
2016-03-13 21:10:27 +0100

    Added setUp and test (commented for now)

--- tests/integration/httpClient/HttpClientTest.php
+++ tests/integration/httpClient/HttpClientTest.php
@@ -1,10 +1,27 @@
 <?php
 
+use BulkSmsCenter\Exceptions\HttpClientException;
+
 class HttpClientTest extends BaseTest
 {
+    protected $exception;
+
+    public function setUp()
+    {
+        parent::setup();
+        $this->exception = "{$this->namespace}\Exceptions\HttpClientException";
+    }
+
     public function testConstructor()
     {
         $httpClient = new \BulkSmsCenter\HttpClient($this->mockAuth);
         $this->assertInstanceOf("{$this->namespace}\HttpClient",$httpClient);
     }
+
+//    public function testInvalidCommand()
+//    {
+//        $httpClient = new \BulkSmsCenter\HttpClient($this->mockAuth);
+//        $this->expectException($this->exception,HttpClientException::CODE__NO_OK_RECEIVED);
+//        $httpClient->runCommand('test_command');
+//    }
 }

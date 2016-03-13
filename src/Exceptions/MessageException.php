c32491bbe082e52ca05297eacaa44454283e53a2
tswestendorp <contact@tswestendorp.nl>
2016-03-13 19:20:53 +0100

    Initial Commit

---
+++ src/Exceptions/MessageException.php
@@ -0,0 +1,10 @@
+<?php namespace BulkSmsCenter\Exceptions;
+
+/**
+ * Class MessageException
+ *
+ * @package BulkSmsCenter\Exceptions
+ */
+class MessageException extends BulkSmsCenterException
+{
+}

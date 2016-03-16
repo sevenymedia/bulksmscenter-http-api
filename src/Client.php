2ffe7db263958602e194e1c1f016b5650d5a37a1
tswestendorp <contact@tswestendorp.nl>
2016-03-16 10:51:14 +0100

    Version bump

--- src/Client.php
+++ src/Client.php
@@ -4,7 +4,7 @@ use BulkSmsCenter\Exceptions\ClientException;
 
 class Client
 {
-    const VERSION = '0.1.1';
+    const VERSION = '1.0.1';
 
     const RESPONSE_KEY__CODE = 'APIcode';
     const RESPONSE_KEY__CREDITS = 'APIcredits';
